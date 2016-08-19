1b901d6c24ad1491cc139e9fd6326974d37917ae
Muni Swetha <muniswetha.mokkala@campsystems.com>
2016-08-19 14:30:54 +0530

    Made changes to support Chrome

--- WCC12-08-2016/IndexPageRequestHandler.js
+++ WCC12-08-2016/IndexPageRequestHandler.js
@@ -2,12 +2,13 @@
 function displayIndexPage(postData, res) 
 {
     fileSystemModule.readFile("./Index.html", function (err, data) {
+        //console.log('adsfadsfadsfasdf');
         if (err) {
             res.writeHead(500, "{Content-Type : text/plain}");
             res.write("Following Error Occured {0}", err);
             res.end();
         }
-        res.writeHead(200, "{Content-Type:text/html}");
+        res.writeHead(200, { "Content-Type": "text/html" });
         res.end(data, 'utf-8');
     });
 }
